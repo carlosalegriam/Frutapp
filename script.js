28292f5762c19f8eb66e325dcdf6419717c05ce0
Carlos Alegria Moena <128553094+carlosalegriam@users.noreply.github.com>
2023-04-01 14:37:19 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function comprar() {
+  alert("Gracias por preferirnos");
+}
+
+var compra = document.getElementsByClassName("link")[0];
+
+acerca.addEventListener("click", function() {
+  comprar();
+});
